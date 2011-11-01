5300b2f1f4a23e0793204624f19f819e1a7fcc79
andrew_kittredge <andrewlkittredge@gmail.com>
2011-11-01 19:22:10 -0400

    World updates from Serge.

--- wumpus/wumpusworld.py
+++ wumpus/wumpusworld.py
@@ -1,18 +1,18 @@
-
 BREEZY,STENCH,GLITER,SCREAM,BUMP,NUM_PERCEPTS = 0,1,2,3,4,5
-TURN_LEFT,TURN_RIGHT,ADVANCE,SHOOT = 0,1,2,3
-LEFT,RIGHT = 0,1
+TURN_LEFT,TURN_RIGHT,ADVANCE,SHOOT,CLIMB,GRAB = 0,1,2,3,4,5
+LEFT,RIGHT = TURN_LEFT,TURN_RIGHT
+
 bumpOnly = [False]*NUM_PERCEPTS
 bumpOnly[BUMP] = True
 
-turns = {( 0, 1):{LEFT:(),RIGHT:()},
-         ( 0,-1):{LEFT:(),RIGHT:()},
-         ( 1, 0):{LEFT:(),RIGHT:()},
-         (-1, 0):{LEFT:(),RIGHT:()},}
+turns = {( 0, 1):{LEFT:(-1, 0),RIGHT:( 1, 0)},
+         ( 0,-1):{LEFT:( 1, 0),RIGHT:(-1, 0)},
+         ( 1, 0):{LEFT:( 0, 1),RIGHT:( 0,-1)},
+         (-1, 0):{LEFT:( 0,-1),RIGHT:(-1, 0)}}
 
 class WumpusWorld:
     def __init__(self, numRows, numCols, wumpusLocation, goldLocation, pitLocations,
-                 agentLocation = (0,0), agentDirection = (1,0)):
+                 agentLocation = (1,1), agentDirection = (1,0)):
         self.percepts = [[[False]*NUM_PERCEPTS for x in range(numCols+2)] for y in range(numRows+2)]
         for percept,(row,col) in zip([STENCH        ] + [BREEZY]*len(pitLocations),
                                      [wumpusLocation] + list(pitLocations)):
@@ -25,33 +25,61 @@ class WumpusWorld:
         for (row,col) in sum([((row,0),(row,-1)) for row in range(numRows+2)] +
                              [((0,col),(-1,col)) for col in range(numCols+2)]):
             self.percepts[row][col-1] = bumpOnly
-        self.agent = Agent(tuple(agentLocation), tuple(agentDirection),
-                           self.getPercept(*agentLocation), 0, 0)
-        self.time  = 0
+        self.numRows, self.numCols = numRows, numCols
+        self.wumpusLocation, self.goldLocation, self.pitLocations, self.entaranceLocation = wumpusLocation, goldLocation, pitLocations, agentLocation
+        self.justKilledWumpus = False
         self.score = 0
+        self.agent = HybridWumpusAgent(initialKnowledgeBase, tuple(agentLocation), tuple(agentDirection))
 
     def getPercept(row, col):
         return tuple(self.percepts[row][col])
 
     def __call__(self)
-        time  += 1
         score -= 10
-        action = self.agent.ask()
-        newRow,  newCol  = self.agent.location
-        newDirX, newDirY = self.agent.agentDirection
-        percept = self.getPercept(newRow, newCol)
-        if action == TURN_LEFT:  newDirX, newDirY = turns[(newDirX, newDirY)][LEFT]
-        if action == TURN_RIGHT: newDirX, newDirY = turns[(newDirX, newDirY)][RIGHT]
-        if action == ADVANCE:
-            newRow, newCol, percept = newRow+newDirX, newCol+newDirY, self.getPercept(newRow,newCol)
-        if newRow == 0 or newCol == 0 or newRow == self.numRows-1 or newCol == self.numCols-1:
-            newRow, newCol, percept = newRow - newDirX, newCol - newDirY, bumpOnly
-        if action == SHOOT:
-        if action == GRAB: 
+        posRow,  posCol  = self.agent.loc
+        dirRow,  dirCol  = self.agent.dir
+        percept = self.getPercept(posRow, posCol)
+        if self.justKilledWumpus:
+            percept[SCREAM] = True
+            self.justKilledWumpus = False
+        if percept == bumpOnly:
+            self.agent.pos = (posRow-dirRow, posCol-dirCol)
+        if (posRow,posCol) == self.wumpusLocation:
+            score -= 1000
+            return "LOOSE"
+        if (posRow,posCol) in self.pitLocations:
+            score -= 1000
+            return "LOOSE"
+        action  = self.agent.ask(percept)
+        if action == TURN_LEFT or action == TURN_RIGHT: dirRow, dirCol = turns[(newDirX, newDirY)][action]
+        if action == ADVANCE: posRow, posCol = posRow+dirRow, posCol+dirCol
+        if action == SHOOT and self.agent.hasArrow:
+            self.agent.hasArrow = False
+            self.score -= 100
+            arrowRow, arrowCol = posRow,  posCol
+            while True:
+                try:
+                    arrowRow, arrowCol = arrowRow + dirRow, arrowCol + dirCol
+                    if (arrowRow, arrowCol) == self.wumpusLocation:
+                        self.justKilledWumpus = True
+                except IndexError:
+                    break
+        if action == CLIMB and (posRow, posCol) == self.entranceLocation and self.hasGold:
+            score += 1000
+            return "WIN"
+        if action ==  GRAB and (posRow,  posCol) == self.glodLocation:
+            self.hasGold = True
+            
+            
     
-def getPercept(world, (row, col)):
-    current = world[row][col]
-    neighbors = 
+def neighboring_coords(coord):
+    x , y = coord[0:2]
+    yield (x - 1, y)
+    yield (x + 1, y)
+    yield (x, y - 1)
+    yield (x, y + 1)
+    
+
 
 
 
