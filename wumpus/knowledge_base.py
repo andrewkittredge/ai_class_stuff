1464e77c2258db56212ff0b734d229214178537b
andrew_kittredge <andrewlkittredge@gmail.com>
2011-11-01 19:26:22 -0400

    initial commit of kb

---
+++ wumpus/knowledge_base.py
@@ -0,0 +1,32 @@
+'''
+Created on Nov 1, 2011
+
+@author: andrewkittredge
+'''
+from wumpusworld import BREEZY, STENCH, neighboring_coords
+ 
+from exceptions import NotImplementedError
+
+class KnowledgeBase(object):
+    def __init__(self, world):
+        self.knowledge = {}
+        self.world = world
+        
+    def ask_ok(self, ok_coord, time):
+        for coord in neighboring_coords(ok_coord):
+            for t in range(t):
+                percept = (coord, t)
+                previous_percept = self.knowledge.get(percept)
+                safe = (previous_percept and STENCH not in previous_percept and BREEZY not in previous_percept)
+                if safe:
+                    return True 
+        return False
+            
+    def ask_visited(self, coord, t):
+        return (coord, t) in self.knowledge
+    
+    
+    def tell_percept(self, percept, coord, time):
+        self.knowledge[(coord, time)] = percept
+    def tell_action(self, action, t):
+        raise NotImplementedError
